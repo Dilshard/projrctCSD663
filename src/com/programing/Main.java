0ac17e91f7ae6d54e89fe1d5f25ef1299eb528f6
macbookpro <mrdilshard@gmail.com>
2020-08-09 15:17:36 +0530

    Added Student cal code

--- src/com/programing/Main.java
+++ src/com/programing/Main.java
@@ -3,6 +3,12 @@ package com.programing;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int mk1 = 70;
+        int mk2 = 80;
+        int mk3 = 90;
+        int tot = mk1 + mk2 + mk3;
+
+        System.out.println("Total :" + tot);
+
     }
 }
